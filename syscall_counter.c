3e795f4204e7457e6db5ddbb410412a9b37dbf2c
afmireski <afmireski@outlook.com>
2024-09-03 19:18:38 -0300

    feat: Implementado incremento do contador quando sys_call é interceptada

--- syscall_counter.c
+++ syscall_counter.c
@@ -1,7 +1,33 @@
+/**
+ * Módulo de Contador de Syscalls
+ * Autores: André Felipe Mireski, Alexandre Tolomeotti, Victor Ângelo Souza Santos
+ */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kprobes.h>
 #include <linux/uaccess.h> // Para funções de manipulação de memória
+#include <linux/proc_fs.h>    // Para criar arquivos no /proc
+#include <linux/sched.h>      // Para obter informações sobre processos
+
+#define PROC_NAME "sys_newuname_counter"
+static struct proc_dir_entry *proc_file;
+
+// Contador de syscalls
+static unsigned long syscall_count = 0;
+
+// Função de callback que será chamada quando o arquivo /proc/syscall_counter for lido
+static ssize_t proc_read(struct file *file, char __user *buf, size_t count, loff_t *pos) {
+    char buffer[64];
+    int len = sprintf(buffer, "Syscall count: %lu\n", syscall_count);
+    
+    return simple_read_from_buffer(buf, count, pos, buffer, len);
+}
+
+// Estrutura de file_operations para mapear a função de leitura
+static struct proc_ops proc_fops = {
+    .proc_read = proc_read,
+};
 
 static struct kprobe kp = {
     .symbol_name = "__x64_sys_newuname",  // Nome da função que queremos monitorar
@@ -22,6 +48,8 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs)
     } else {
         printk(KERN_INFO "sys_newuname chamada com filename não acessível\n");
     }
+    
+    syscall_count++;
 
     return 0;
 }
@@ -40,6 +68,14 @@ static int __init kprobe_init(void)
     }
 
     printk(KERN_INFO "kprobe registrada para sys_newuname\n");
+
+    // Cria o arquivo /proc/syscall_counter
+    proc_file = proc_create(PROC_NAME, 0, NULL, &proc_fops);
+    if (!proc_file) {
+        return -ENOMEM;
+    }
+    
+    printk(KERN_INFO "Syscall Counter Module Loaded\n");
     return 0;
 }
 
@@ -48,6 +84,8 @@ static void __exit kprobe_exit(void)
 {
     unregister_kprobe(&kp);
     printk(KERN_INFO "kprobe desregistrada\n");
+    proc_remove(proc_file);
+    printk(KERN_INFO "Syscall Counter Module Unloaded\n");
 }
 
 module_init(kprobe_init);
